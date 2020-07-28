80dc675645429b3d78764c3ab6e7d1e02f88ee2e
rsnazario <rafael.s.nazario@gmail.com>
2020-07-28 18:25:01 -0300

    app.js linter fix

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-let myLibrary = [];
+const myLibrary = [];
 
 function Book(name, author, status) {
   this.name = name;
@@ -7,12 +7,12 @@ function Book(name, author, status) {
 }
 
 function addBookToLibrary() {
-  let addForms = document.forms['add-book'];
-  addForms.addEventListener("submit", function(event) {
+  const addForms = document.forms['add-book'];
+  addForms.addEventListener('submit', (event) => {
     event.preventDefault();
     const nameValue = addForms.querySelector('#name').value;
     const authorValue = addForms.querySelector('#author').value;
-    let arr = [nameValue, authorValue, false];
+    const arr = [nameValue, authorValue, false];
     myLibrary.push(arr);
     render();
   });
@@ -24,7 +24,7 @@ function deleteBook(e) {
   if (el.className == 'delete') {
     const li = el.parentElement; // get the whole li item that encapsulates the delete
     // Array method that gets the nth-child (in this case, the nth-LI on the UL parent)
-    let position = Array.prototype.indexOf.call(ul.children, li);
+    const position = Array.prototype.indexOf.call(ul.children, li);
     ul.removeChild(li); // html dealing
     myLibrary.splice(position, 1); // array dealing
   }
@@ -32,7 +32,7 @@ function deleteBook(e) {
 
 function toggleRead(e) {
   let el = e.target.checked;
-  el = !el;  // list = document.querySelector('#book-list ul');  // list.addEventListener('click', function(toggle){
+  el = !el; // list = document.querySelector('#book-list ul');  // list.addEventListener('click', function(toggle){
   //   toggle.preventDefault();
   //   if (toggle.target.className == 'read-button'){
   //     let li = toggle.target.parentElement;
@@ -44,25 +44,24 @@ function toggleRead(e) {
 
 function render() {
   const list = document.querySelector('#book-list ul');
-  let i = myLibrary.length - 1;
+  const i = myLibrary.length - 1;
   // Create Elements
   const li = document.createElement('li');
   const bookName = document.createElement('span');
   const authorName = document.createElement('span');
   const readbox = document.createElement('input');
-  readbox.type = "checkbox";
-  readbox.setAttribute("onclick", "toggleRead(event)");
+  readbox.type = 'checkbox';
+  readbox.setAttribute('onclick', 'toggleRead(event)');
   const deleteBtn = document.createElement('span');
   // Add content
   bookName.textContent = myLibrary[i][0];
   authorName.textContent = myLibrary[i][1];
-  console.log(myLibrary[i][2]);
-  readbox.checked = myLibrary[i][2];  deleteBtn.textContent = 'delete';
-  deleteBtn.setAttribute("onclick", "deleteBook(event)");
+  readbox.checked = myLibrary[i][2]; deleteBtn.textContent = 'delete';
+  deleteBtn.setAttribute('onclick', 'deleteBook(event)');
   // Add classes
-  bookName.classList.add('name')
-  authorName.classList.add('author')
-  deleteBtn.classList.add('delete')
+  bookName.classList.add('name');
+  authorName.classList.add('author');
+  deleteBtn.classList.add('delete');
   readbox.classList.add('read-button');
   // Append to document
   li.appendChild(bookName);
@@ -73,5 +72,5 @@ function render() {
 }
 
 addBookToLibrary();
-//deleteBook();
-//toggleRead();
+// deleteBook();
+// toggleRead();
