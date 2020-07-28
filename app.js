4995f6db75acf0ad53813ca66cc38a77616cade1
kwizl <karis.njoroge@gmail.com>
2020-07-29 00:09:32 +0300

    Add read checkbox to show read status

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ function Book(name, author, status) {
   this.status = status;
 }
 
-function addBookToLibrary() {  
+function addBookToLibrary() {
   let addForms = document.forms['add-book'];
   addForms.addEventListener("submit", function(event) {
     event.preventDefault();
@@ -18,32 +18,28 @@ function addBookToLibrary() {
   });
 }
 
-function deleteBook() {
-  list = document.querySelector('#book-list ul');
+function deleteBook(e) {
+  const ul = document.querySelector('#book-list ul');
+  const el = e.target;
+  if (el.className == 'delete') {
+    const li = el.parentElement; // get the whole li item that encapsulates the delete
+    // Array method that gets the nth-child (in this case, the nth-LI on the UL parent)
+    let position = Array.prototype.indexOf.call(ul.children, li);
+    ul.removeChild(li); // html dealing
+    myLibrary.splice(position, 1); // array dealing
+  }
+}
 
-  list.addEventListener('click', function(del) {
-    del.preventDefault();
-    if (del.target.className == 'delete'){
-      const li = del.target.parentElement; // get the whole li item that encapsulates the delete
-      // Array method that gets the nth-child (in this case, the nth-LI on the UL parent)
-      let position = Array.prototype.indexOf.call(list.children, li);
-      list.removeChild(li); // html dealing
-      myLibrary.splice(position, 1); // array dealing
-    }
-  });
-};
-
-function toggleRead() {
-  list = document.querySelector('#book-list ul');
-
-  list.addEventListener('click', function(toggle){
-    toggle.preventDefault();
-    if (toggle.target.className == 'read-button'){
-      let li = toggle.target.parentElement;
-      let position = Array.prototype.indexOf.call(list.children, li);
-      myLibrary[position][2] = true;
-    }
-  })
+function toggleRead(e) {
+  let el = e.target.checked;
+  el = !el;  // list = document.querySelector('#book-list ul');  // list.addEventListener('click', function(toggle){
+  //   toggle.preventDefault();
+  //   if (toggle.target.className == 'read-button'){
+  //     let li = toggle.target.parentElement;
+  //     let position = Array.prototype.indexOf.call(list.children, li);
+  //     myLibrary[position][2] = true;
+  //   }
+  // })
 }
 
 function render() {
@@ -53,18 +49,16 @@ function render() {
   const li = document.createElement('li');
   const bookName = document.createElement('span');
   const authorName = document.createElement('span');
-  
   const readbox = document.createElement('input');
   readbox.type = "checkbox";
-
+  readbox.setAttribute("onclick", "toggleRead(event)");
   const deleteBtn = document.createElement('span');
   // Add content
   bookName.textContent = myLibrary[i][0];
   authorName.textContent = myLibrary[i][1];
   console.log(myLibrary[i][2]);
-  readbox.checked = myLibrary[i][2];
-
-  deleteBtn.textContent = 'delete';
+  readbox.checked = myLibrary[i][2];  deleteBtn.textContent = 'delete';
+  deleteBtn.setAttribute("onclick", "deleteBook(event)");
   // Add classes
   bookName.classList.add('name')
   authorName.classList.add('author')
@@ -75,9 +69,9 @@ function render() {
   li.appendChild(authorName);
   li.appendChild(readbox);
   li.appendChild(deleteBtn);
-  list.appendChild(li); 
+  list.appendChild(li);
 }
 
 addBookToLibrary();
-deleteBook();
-toggleRead();
+//deleteBook();
+//toggleRead();
