d35e82eee43d38dfbae20de2685be29b671e379d
kwizl <karis.njoroge@gmail.com>
2020-07-29 14:52:24 +0300

    Edit the display books detail UI

--- js/app.js
+++ js/app.js
@@ -6,9 +6,22 @@ function Book(name, author, status) {
   this.status = status;
 }
 
+function openWindow() {
+  const wrap = document.querySelector('#wrapper');
+  wrap.style.display = 'block';
+}
+
+function closeWindow() {
+  const wrap = document.querySelector('#wrapper');
+  wrap.style.display = 'none';
+}
+
+const titleBtn = document.querySelector('.title-btn button');
+titleBtn.onclick = () => openWindow();
+
 // eslint-disable-next-line no-unused-vars
 function deleteBook(e) {
-  const ul = document.querySelector('#book-list ul');
+  const ul = document.querySelector('.table tbody');
   const el = e.target;
   if (el.className === 'delete') {
     const li = el.parentElement;
@@ -24,13 +37,14 @@ function toggleRead(e) {
 }
 
 function render() {
-  const list = document.querySelector('#book-list ul');
+  const list = document.querySelector('.table tbody');
   const i = myLibrary.length - 1;
-  const li = document.createElement('li');
-  const bookName = document.createElement('span');
-  const authorName = document.createElement('span');
+  const li = document.createElement('tr');
+  const bookName = document.createElement('td');
+  const authorName = document.createElement('td');
+  const tdInput = document.createElement('td');
   const readbox = document.createElement('input');
-  const deleteBtn = document.createElement('span');
+  const deleteBtn = document.createElement('td');
 
   readbox.type = 'checkbox';
   readbox.setAttribute('onclick', 'toggleRead(event)');
@@ -48,7 +62,8 @@ function render() {
 
   li.appendChild(bookName);
   li.appendChild(authorName);
-  li.appendChild(readbox);
+  tdInput.appendChild(readbox);
+  li.appendChild(tdInput);
   li.appendChild(deleteBtn);
   list.appendChild(li);
 }
@@ -62,8 +77,10 @@ function addBookToLibrary() {
     const book = new Book(nameValue, authorValue, false);
     myLibrary.push(book);
     render();
+    closeWindow();
   });
   return myLibrary;
 }
 
+window.onload = closeWindow();
 addBookToLibrary();
